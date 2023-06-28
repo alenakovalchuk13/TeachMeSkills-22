35de13660310cf31beb7d78eb0ad578a40300765
Alena Kovalchuk <alenakovalchuk13@gmail.com>
2023-06-28 21:22:03 +0300

    hm 11 done

--- src/TasksMain/Document.java
+++ src/TasksMain/Document.java
@@ -1,27 +1,27 @@
 package TasksMain;
 
 public class Document {
-    public static void check(String string) {
+    public static void check(String string) throws ExceptionABC{
         if (string.contains("abc") || string.contains("ABC")) {
             System.out.println("The document number contains the sequence - \"abc\".");
         } else {
-            System.out.println("The document number does not contain the sequence - \"abc\".");
+            throw new ExceptionABC("The document number does not contain the sequence - \"abc\".");
         }
     }
 
-    public static void check555(String string) {
+    public static void check555(String string) throws Exception555{
         if (string.substring(0,3).equals("555")) {
             System.out.println("The document number starts with 555.");
         } else {
-            System.out.println("The document number does not start with 555.");
+            throw new Exception555("The document number does not start with 555.");
         }
     }
 
-    public static void check1a2b(String string) {
+    public static void check1a2b(String string) throws Exception1a2b{
         if (string.substring(string.length() - 4).equals("1a2b")) {
             System.out.println("The document number ends with 1a2b.");
         } else {
-            System.out.println("The document number does not end with 1a2b.");
+            throw new Exception1a2b("The document number does not end with 1a2b.");
         }
     }
 }
--- src/TasksMain/Main.java
+++ src/TasksMain/Main.java
@@ -1,8 +1,30 @@
 package TasksMain;
+import java.io.IOException;
+
+import static TasksMain.Document.*;
 
 public class Main {
     public static void main(String[] args) {
+        String string = "5467-hgf-9845-kju-1a2b";
 
+        try {
+            check(string);
+        } catch (ExceptionABC e) {
+            e.getStackTrace();
+        }
 
+        try {
+            check555(string);
+        } catch (Exception555 e) {
+            e.getStackTrace();
+        }
+
+        try {
+            check1a2b(string);
+        } catch (Exception1a2b e) {
+            e.getStackTrace();
+
+        }
+        }
     }
-}
+
